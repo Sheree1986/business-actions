c830c7a8e5958bbf08f70102cc17486717c52f21
Sheree1986 <sheree.edmund@gmail.com>
2023-02-09 13:09:05 -0500

    add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,9 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
-
+it('should do something amazing',  () => {
+  expect(true).toBe(false);
+});
 it('should render', () => {
   render(<App />);
 
